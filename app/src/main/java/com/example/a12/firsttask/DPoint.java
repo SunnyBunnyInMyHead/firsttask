22d8a87bccc507f0bca98866fc120a2fd1336fe4
SunnyBunnyInMyHead <SkliarovEvhenii@i.ua>
2018-06-12 16:08:17 +0300

    initial

--- app/src/main/java/com/example/a12/firsttask/DPoint.java
+++ app/src/main/java/com/example/a12/firsttask/DPoint.java
@@ -7,4 +7,19 @@ public class DPoint {
        this.y = y;
    }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
--- app/src/main/java/com/example/a12/firsttask/MainActivity.java
+++ app/src/main/java/com/example/a12/firsttask/MainActivity.java
@@ -7,43 +7,44 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-    Button button;
-    EditText innerX0,innerY0,innerX1,innerY1;
+    private Button button;
+    EditText innerX0, innerY0, innerX1, innerY1;
     TextView outX, outY;
     TextView outLength;
     Spinner spinner, innerspinner;
 
-    int inX1,inX2, inY1,inY2;
-    String[] data = {"1", "2", "3", "4", "5","6", "7", "8", "9"};
-    String[] data2 = {"not define","1", "2", "3", "4", "5","6", "7", "8", "9"};
+    int inX1, inX2, inY1, inY2;
+    String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    String[] data2 = {"not define", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     List<DPoint> list = new ArrayList();
 
     int dimensionOfArea = 1000;
 
 
     String resX, resY;
-    public void fillingList(){
+
+    public void fillingList() {
         list.add(null);//0
-        list.add(new DPoint(1.0/6, 1.0/6));//1
-        list.add(new DPoint(3.0/6, 1.0/6));//2
-        list.add(new DPoint(5.0/6, 1.0/6));//3
-        list.add(new DPoint(5.0/6, 3.0/6));//4
-        list.add(new DPoint(5.0/6, 5.0/6));//5
-        list.add(new DPoint(3.0/6, 5.0/6));//6
-        list.add(new DPoint(1.0/6, 5.0/6));//7
-        list.add(new DPoint(1.0/6, 3.0/6));//8
-        list.add(new DPoint(3.0/6, 3.0/6));//9
+        list.add(new DPoint(1.0 / 6, 1.0 / 6));//1
+        list.add(new DPoint(3.0 / 6, 1.0 / 6));//2
+        list.add(new DPoint(5.0 / 6, 1.0 / 6));//3
+        list.add(new DPoint(5.0 / 6, 3.0 / 6));//4
+        list.add(new DPoint(5.0 / 6, 5.0 / 6));//5
+        list.add(new DPoint(3.0 / 6, 5.0 / 6));//6
+        list.add(new DPoint(1.0 / 6, 5.0 / 6));//7
+        list.add(new DPoint(1.0 / 6, 3.0 / 6));//8
+        list.add(new DPoint(3.0 / 6, 3.0 / 6));//9
     }
 
-    public void initialization(){
+    public void initialization() {
         innerX0 = (EditText) findViewById(R.id.editText8);
         innerY0 = (EditText) findViewById(R.id.editText10);
         innerX1 = (EditText) findViewById(R.id.editText9);
@@ -86,8 +87,8 @@ public class MainActivity extends Activity {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 // показываем позиция нажатого элемента
-               // ( (TextView)findViewById(R.id.textView26)).setText(String.valueOf(position+1));
-               // outLength.setText(String.valueOf(calculatelength()));
+                // ( (TextView)findViewById(R.id.textView26)).setText(String.valueOf(position+1));
+                // outLength.setText(String.valueOf(calculatelength()));
             }
 
             @Override
@@ -109,9 +110,7 @@ public class MainActivity extends Activity {
         });
 
 
-
-        button =(Button) findViewById(R.id.button11);
-
+        button = (Button) findViewById(R.id.button11);
 
 
         View.OnClickListener oclBtnOk = new View.OnClickListener() {
@@ -122,11 +121,10 @@ public class MainActivity extends Activity {
                 outLength.setText(String.valueOf(calculatelength()));
 
 
-               // outY.setText(String.valueOf(center.y));
+                // outY.setText(String.valueOf(center.y));
 
-                outX.setText(innerX0.getText().toString()+" "+ innerX1.getText().toString() +" "+ String.valueOf((int)center.x));
-                outY.setText(innerY0.getText().toString()+" "+ innerY1.getText().toString() +" "+ String.valueOf((int)center.y));
-
+                outX.setText(innerX0.getText().toString() + " " + innerX1.getText().toString() + " " + String.valueOf((int) center.x));
+                outY.setText(innerY0.getText().toString() + " " + innerY1.getText().toString() + " " + String.valueOf((int) center.y));
 
 
             }
@@ -135,29 +133,29 @@ public class MainActivity extends Activity {
 
     }
 
-    public double calculatelength(){
-        if(list.get(innerspinner.getSelectedItemPosition())!=null){
-            return dimensionOfArea/9;
-        }else {
-            return dimensionOfArea/3;
+    public double calculatelength() {
+        if (list.get(innerspinner.getSelectedItemPosition()) != null) {
+            return dimensionOfArea / 9;
+        } else {
+            return dimensionOfArea / 3;
         }
     }
 
-    public DPoint calculatecoordinates(int sizeOfArea){
-        Double x=0.0,y=0.0;
+    public DPoint calculatecoordinates(int sizeOfArea) {
+        Double x = 0.0, y = 0.0;
         int externalpos = Integer.valueOf(spinner.getSelectedItemPosition());
         int innerpos = Integer.valueOf(innerspinner.getSelectedItemPosition());
 
-        if(list.get(innerpos)==null){
-            x=sizeOfArea*list.get (externalpos+1).x;
-            y=sizeOfArea*list.get(externalpos+1).y;
-            return new DPoint(x,y);
-        }else {
-            x=(sizeOfArea*list.get(externalpos+1).x-sizeOfArea/6)+
-                    list.get(innerpos).x*sizeOfArea/3;
-            y=(sizeOfArea*list.get(externalpos+1).y-sizeOfArea/6)+
-                    list.get(innerpos).y*sizeOfArea/3;
-            return new DPoint(x,y);
+        if (list.get(innerpos) == null) {
+            x = sizeOfArea * list.get(externalpos + 1).x;
+            y = sizeOfArea * list.get(externalpos + 1).y;
+            return new DPoint(x, y);
+        } else {
+            x = (sizeOfArea * list.get(externalpos + 1).x - sizeOfArea / 6) +
+                    list.get(innerpos).x * sizeOfArea / 3;
+            y = (sizeOfArea * list.get(externalpos + 1).y - sizeOfArea / 6) +
+                    list.get(innerpos).y * sizeOfArea / 3;
+            return new DPoint(x, y);
         }
 
     }
